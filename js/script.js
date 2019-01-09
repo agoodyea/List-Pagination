cd608e8ceaf5679f7a32b7748e29f1be5b38ccef
Aiden Goodyear <agoodyea30@gmail.com>
2019-01-09 10:40:36 +0800

    added link active class functionality

--- js/script.js
+++ js/script.js
@@ -5,52 +5,26 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-/*** 
-   I need to make it so
-***/
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 // Global Variable
-// - My initial thoughts is that i need to create a studentList global variable.
-// - That way I can have all students saved in one DOM variable and manipulate which ones are displayed.
 
 const list = document.getElementsByClassName('student-list')[0].children;
 const numOfPages = Math.ceil(list.length / 10);
+const div = document.createElement('div');
+div.className = 'pagination';
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 // ShowPage Function
-// - This function needs to take the student list and page number and display the 10 li's for that page.
-// - when the next page number button is clicked, the next 10 items will be displayed.
+/*** 
+   This function takes a list and page
+***/
 
-const showPages = (list, page) => {
+const showPage = (list, page) => {
    const startPoint = 10 * (page - 1);
    const endPoint = startPoint + 10;
    for (let i = 0; i < list.length; i += 1) {
       let li = list[i]
-      if (i >= startPoint && i <= endPoint) {
+      if (i >= startPoint && i < endPoint) {
          li.style.display = '';
       } else {
          li.style.display = 'none';
@@ -58,50 +32,51 @@ const showPages = (list, page) => {
    }
 };
 
-
-
-
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 // Append Page Links Function
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+
 ***/
 
 
 const appendPageLinks = (list) => {
-   // dom elements
-   pageDiv = document.querySelector('.page');
-   div = document.createElement('div');
-   div.className = 'pagination';
-   ul = document.createElement('ul');
+   // create dom elements
+   const pageDiv = document.querySelector('.page');
+   const ul = document.createElement('ul');
+   const numOfPages = Math.ceil(list.length / 10);
    // create li + a elements and append to ul
    for (let i = 0; i < numOfPages; i += 1) {
       let li = document.createElement('li');
       let a = document.createElement('a');
       a.textContent = i + 1;
+      if (i === 0) {
+         a.className = 'active';
+      }
       li.appendChild(a);
       ul.appendChild(li)
    };
-   // append up to div and div to pageDiv
+   // append ul to div and div to pageDiv
    div.appendChild(ul)
    pageDiv.appendChild(div);
-   firstPage = parseInt(ul.firstElementChild.firstElementChild.textContent);
-   showPages(list, firstPage);
+   firstPage = ul.firstElementChild.firstElementChild.textContent;
+   showPage(list, firstPage);
 };
 
 appendPageLinks(list);
 
 div.addEventListener('click', (e) => {
-   number = e.target.textContent;
-   number.className = 'activated';
-   showPages(list, number);
-
-
-   
+   linkList = e.target.parentElement.parentElement.children;
+   link = e.target;
+   number = link.textContent;
+   for (let i = 0; i < linkList.length; i += 1) {
+      a = linkList[i].firstElementChild;
+      a.className ='';
+   }
+   link.className = 'active';
+   showPage(list, number);
 });
 
 
